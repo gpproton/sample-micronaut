caa136abbe6355c0c710ef916e0b7e4e822a84bf
bit Overloader <me@godwin.dev>
2020-12-21 18:25:40 +0000

    Initial commit

---
+++ src/test/groovy/cloud/elevens/torque/SampleMicronautSpec.groovy
@@ -0,0 +1,19 @@
+package cloud.elevens.torque
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class SampleMicronautSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
